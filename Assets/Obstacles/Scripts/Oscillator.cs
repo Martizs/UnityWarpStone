5370506edd3fa9c249f5af78f6cfa33e2410cbba
Martynas <martynas@sdhouse.lt>
2024-01-28 14:50:39 +0200

    Obstacle movement done. Start of obstacle control aura

--- Assets/Obstacles/Scripts/Oscillator.cs
+++ Assets/Obstacles/Scripts/Oscillator.cs
@@ -15,6 +15,18 @@ public class Oscillator : MonoBehaviour
     [SerializeField]
     float period = 2f;
 
+    [SerializeField]
+    float shakeSpeed = 1f;
+
+    [SerializeField]
+    float shakeAmount = 1f;
+
+    [SerializeField]
+    float shakeRotateSpeed = 1f;
+
+    [SerializeField]
+    Vector3 shakeRotateVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +35,13 @@ public class Oscillator : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        ObstacleMovement();
+        Shake();
+        RotationShake();
+    }
+
+    void ObstacleMovement()
     {
         // continualy growing over time
         float cycles = Time.time / (period <= Mathf.Epsilon ? 1 : period);
@@ -34,5 +53,19 @@ public class Oscillator : MonoBehaviour
 
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
+    }
+
+    void Shake()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + Mathf.Sin(Time.time * shakeSpeed) * shakeAmount,
+            transform.position.z
+        );
+    }
+
+    void RotationShake()
+    {
+        transform.Rotate(shakeRotateVector * Mathf.Sin(Time.time * shakeRotateSpeed));
     }
 }
